5e3e3da807a287d8e18e87e53cff2f9a26cfddb0
AndyCao <224799982+GreatAndyC@users.noreply.github.com>
2025-09-13 17:45:20 +0800

    Add Alien class and image. fix alien creation and movement logic

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -3,6 +3,7 @@ import sys
 from settings import Settings
 from ship import Ship
 from bullet import Bullet
+from alien import Alien
 
 
 class AlienInvasion:
@@ -23,6 +24,9 @@ class AlienInvasion:
 
         self.ship = Ship(self) #创建飞船实例
         self.bullets = pygame.sprite.Group() #创建一个用于存储子弹的编组
+        self.aliens = pygame.sprite.Group() #创建一个用于存储外星人的编组
+
+        self._create_fleet() #创建外星人群
 
         #设置背景颜色
         self.bg_color = (230,230,230) #RGB颜色浅灰色
@@ -33,6 +37,9 @@ class AlienInvasion:
             self.ship.update()
             self.bullets.update()
             self._check_events()
+            self._update_bullets()
+            self._update_aliens()
+
             self._update_screen()
 
     def _check_events(self):
@@ -66,8 +73,19 @@ class AlienInvasion:
 
     def _fire_bullet(self):
         """创建一颗子弹，并将其加入编组bullets中"""
-        new_bullet = Bullet(self) #创建一个子弹实例
-        self.bullets.add(new_bullet) #将子弹加入到编组中
+        if len(self.bullets) < self.settings.bullet_allowed: #如果屏幕上子弹数量超过允许的数量
+            new_bullet = Bullet(self) #创建一个子弹实例
+            self.bullets.add(new_bullet) #将子弹加入到编组中
+
+    def _update_bullets(self):
+        """更新子弹的位置，并删除已消失的子弹"""
+        #更新子弹的位置
+        self.bullets.update()
+
+        #删除已消失的子弹
+        for bullet in self.bullets.copy(): #遍历编组中的每颗子弹的副本
+            if bullet.rect.bottom <= 0: #如果子弹的底部小于等于0
+                self.bullets.remove(bullet) #将子弹从编组中删除
 
     def _update_screen(self):
         #每次循环时都重绘屏幕,并切换到新屏幕
@@ -75,10 +93,56 @@ class AlienInvasion:
         self.ship.blitme() #在指定位置绘制飞船
         for bullet in self.bullets.sprites(): #遍历编组中的每颗子弹
             bullet.draw_bullet() #在屏幕上绘制子弹
+        self.aliens.draw(self.screen) #在屏幕上绘制外星人
         #让最近绘制的屏幕可见
         pygame.display.flip()
 
+    def _create_fleet(self):
+        """创建外星人群"""
+        #创建一个外星人，并计算一行可容纳多少个外星人
+        #外星人间距为外星人宽度
+        alien = Alien(self) 
+        alien_width, alien_height = alien.rect.size
+        available_space_x = self.settings.screen_width - (2 * alien_width) #屏幕宽度-2个外星人宽度
+        number_aliens_x = available_space_x // (2 * alien_width) #每个外星人占2个外星人宽度
+
+        #计算屏幕可以容纳多少行外星人
+        ship_height = self.ship.rect.height
+        available_space_y = (self.settings.screen_height -
+            (3 * alien_height) - ship_height)
+        number_rows = available_space_y // (2 * alien_height)
+
+        #创建外星人群
+        for row_number in range(number_rows):
+            for alien_number in range(number_aliens_x):
+                self._create_alien(alien_number, row_number)
+
+    def _create_alien(self, alien_number, row_number):
+        """创建一个外星人并将其放在当前行"""
+        alien = Alien(self)
+        alien_width, alien.rect.height = alien.rect.size
+        alien.x = alien_width + 2 * alien_width * alien_number
+        alien.rect.x = alien.x
+        alien.rect.y = alien.rect.height + 2 * alien.rect.height * row_number
+        self.aliens.add(alien) #加入编组
+
+    def _update_aliens(self):
+        """检查是否有外星人位于屏幕边缘，并更新整群外星人的位置"""
+        self._check_fleet_edges()
+        self.aliens.update()
     
+    def _check_fleet_edges(self):
+        """有外星人到达边缘时采取相应的措施"""
+        for alien in self.aliens.sprites():
+            if alien.check_edges():
+                self._change_fleet_direction()
+                break
+    
+    def _change_fleet_direction(self):
+        """将整群外星人下移，并改变它们的方向"""
+        for alien in self.aliens.sprites():
+            alien.rect.y += self.settings.fleet_drop_speed
+        self.settings.fleet_direction *= -1 #改变方向
 
 if __name__ == '__main__':
     #创建游戏实例并运行游戏
--- alien_invasion/settings.py
+++ alien_invasion/settings.py
@@ -9,10 +9,18 @@ class Settings:
         self.bg_color = (230,230,230)
         
         # 飞船设置
-        self.ship_speed = 2
+        self.ship_speed = 1.5
 
         #子弹设置 创建宽3px 高15px的子弹 颜色为深灰色
         self.bullet_speed = 1
         self.bullet_width = 3
         self.bullet_height = 15
         self.bullet_color = (60,60,60)
+        self.bullet_allowed = 3 #屏幕上同时出现的子弹数量
+
+        #外星人设置
+        self.alien_speed = 1.0
+        self.fleet_drop_speed = 10
+        #fleet_direction为1表示向右移，为-1表示向左移
+        self.fleet_direction = 0.5
+
